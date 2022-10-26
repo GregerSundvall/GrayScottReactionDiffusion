ed0201e32dd2fc682728e04bf8bab927f3db5c4d
GregerSundvall <71319618+GregerSundvall@users.noreply.github.com>
2022-10-26 17:01:41 +0200

    bs

--- src/main.lua
+++ src/main.lua
@@ -3,10 +3,10 @@
 
 
 
-local buffer1
-local buffer2
-local buffers = {}
-local nextBuffer = 2
+local buffer = {}
+--local backBuffer
+--local buffers = {}
+--local nextBuffer = 2
 local dA = 1.0
 local bB = 0.5
 local feed = 0.055
@@ -16,37 +16,47 @@ local windowHeight = love.graphics.getHeight()
 
 
 local function InitBuffer(value)
-    local buffer = {}
+    --local buffer = {}
     for i = 1, windowWidth do
         buffer[i] = {}
         for j = 1, windowHeight do
             buffer[i][j] = value
         end
     end
-    return buffer
 end
 
 function love.load()
-    buffer1 = InitBuffer(0)
-    buffer2 = InitBuffer(0)
-    buffers[1] = buffer1
-    buffers[2] = buffer2
+    InitBuffer(1)
+    --backBuffer = InitBuffer(0)
+    --buffers[1] = buffer1
+    --buffers[2] = buffer2
+
 
 end
 
 function love.update()
-    nextBuffer = ((nextBuffer) % 2) + 1
+    --nextBuffer = ((nextBuffer) % 2) + 1
+    --for i = 1, windowWidth do
+    --    for j = 1, windowHeight do
+    --        buffer[i][j] = buffer[i][j] * 0.009
+    --    end
+    --end
 
+
+    --buffer = backBuffer
 end
 
 function love.draw()
-    local buffer = buffers[nextBuffer]
+    --local buffer = buffers[nextBuffer]
+    points = {}
     for i = 1, windowWidth do
         for j = 1, windowHeight do
-            local point = {i, j -0.5, buffer[i][j], buffer[i][j], buffer[i][j], buffer[i][j]}
-            love.graphics.points(point)
+            local point = {i, j, buffer[i][j], buffer[i][j], buffer[i][j], 1}
+            points[i*j] = point
+            --love.graphics.points(i, j, 0.5, 0.5, 0.5, 1)
         end
     end
+    love.graphics.points(points)
 end
 
 
